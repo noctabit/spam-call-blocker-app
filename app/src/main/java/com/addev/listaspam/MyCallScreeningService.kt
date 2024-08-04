34f278a95e7e8249752c4eacf746b1acafcd3347
Adam <adamff.me@gmail.com>
2024-08-04 13:31:24 +0200

    Update MyCallScreeningService to use SpamUtils

--- app/src/main/java/com/addev/listaspam/MyCallScreeningService.kt
+++ app/src/main/java/com/addev/listaspam/MyCallScreeningService.kt
@@ -1,34 +1,67 @@
 package com.addev.listaspam
 
 import android.content.Context
+import android.net.Uri
+import android.os.Build
 import android.telecom.Call
 import android.telecom.CallScreeningService
+import android.telecom.TelecomManager
+import android.telephony.TelephonyManager
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 class MyCallScreeningService : CallScreeningService() {
+
+    val spamUtils = SpamUtils()
 
     companion object {
         private const val SPAM_PREFS = "SPAM_PREFS"
         private const val BLOCK_NUMBERS_KEY = "BLOCK_NUMBERS"
     }
 
-    override fun onScreenCall(callDetails: Call.Details) {
-        val sharedPreferences = getSharedPreferences(SPAM_PREFS, Context.MODE_PRIVATE)
-        val blockedNumbers = sharedPreferences.getStringSet(BLOCK_NUMBERS_KEY, null)
-        val incomingNumber = callDetails.handle.schemeSpecificPart
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun onScreenCall(details: Call.Details) {
+        if (details.callDirection != Call.Details.DIRECTION_INCOMING)
+            return
 
-        if (blockedNumbers?.contains(incomingNumber) == true) {
-            respondToCall(callDetails, CallResponse.Builder()
-                .setDisallowCall(true)
-                .setRejectCall(true)
-                .build())
+        var rawNumber = ""
+        if (details.handle != null) {
+            rawNumber = details.handle.schemeSpecificPart
+        } else if (details.gatewayInfo?.originalAddress != null){
+            rawNumber = details.gatewayInfo?.originalAddress?.schemeSpecificPart!!
+        } else if (details.intentExtras != null) {
+            var uri = details.intentExtras.getParcelable<Uri>(TelecomManager.EXTRA_INCOMING_CALL_ADDRESS)
+            if (uri == null) {
+                uri = details.intentExtras.getParcelable<Uri>(TelephonyManager.EXTRA_INCOMING_NUMBER)
+            }
+            if (uri != null) {
+                rawNumber = uri.schemeSpecificPart
+            }
+        }
+
+        rawNumber?.let {
+            val sharedPreferences = getSharedPreferences(SPAM_PREFS, Context.MODE_PRIVATE)
+            val blockedNumbers = sharedPreferences.getStringSet(BLOCK_NUMBERS_KEY, null)
+
+            if (blockedNumbers?.contains(rawNumber) == true) {
+                endCall(details)
+                return
+            }
+
+            spamUtils.checkSpamNumber(this, it) { isSpam ->
+                if (isSpam) {
+                    endCall(details)
+                }
+            }
         }
     }
 
-    private fun blockCall(callDetails: Call.Details) {
-        val callResponse = CallResponse.Builder()
-            .setDisallowCall(true)
-            .setRejectCall(true)
-            .build()
-        respondToCall(callDetails, callResponse)
+    private fun endCall(details: Call.Details) {
+        respondToCall(
+            details, CallResponse.Builder()
+                .setDisallowCall(true)
+                .setRejectCall(true)
+                .build()
+        )
     }
 }
