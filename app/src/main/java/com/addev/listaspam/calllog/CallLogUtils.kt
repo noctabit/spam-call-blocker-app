10f1cccf33b99a12adc70aa79fc2917b46bc7dde
Adam <adamff.me@gmail.com>
2024-08-07 00:11:17 +0200

    CallLogUtils.kt: Show only incoming calls

--- app/src/main/java/com/addev/listaspam/calllog/CallLogUtils.kt
+++ app/src/main/java/com/addev/listaspam/calllog/CallLogUtils.kt
@@ -15,10 +15,13 @@ data class CallLogEntry(
 
 fun getCallLogs(context: Context): List<CallLogEntry> {
     val callLogs = mutableListOf<CallLogEntry>()
+
+    val selection = "${CallLog.Calls.TYPE} IN (${CallLog.Calls.INCOMING_TYPE}, ${CallLog.Calls.REJECTED_TYPE}, ${CallLog.Calls.BLOCKED_TYPE})"
+
     val cursor: Cursor? = context.contentResolver.query(
         CallLog.Calls.CONTENT_URI,
         null,
-        null,
+        selection,
         null,
         CallLog.Calls.DATE + " DESC"
     )
