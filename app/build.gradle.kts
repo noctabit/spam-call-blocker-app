d268c5d6f20986fefc6a8a7a7b30165be6c67af3
Adam <adamff.me@gmail.com>
2025-06-03 22:53:09 +0200

    build.gradle.kts: Update versionCode to 14 and versionName to 2.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.addev.listaspam"
         minSdk = 29
         targetSdk = 34
-        versionCode = 13
-        versionName = "2.2.2"
+        versionCode = 14
+        versionName = "2.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
