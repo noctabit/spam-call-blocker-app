a195679e55322c11237fc8950dadb5929032e61a
Adam <adamff.me@gmail.com>
2025-06-06 14:22:41 +0200

    build.gradle.kts: Update version code and name to 18 and 2.3.2 respectively

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         minSdk = 29
         //noinspection OldTargetApi
         targetSdk = 34
-        versionCode = 16
-        versionName = "2.3.0"
+        versionCode = 18
+        versionName = "2.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
